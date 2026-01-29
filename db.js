e685c5ecfbfe39118dc6b68677e0326189ccae78
Gubbala Govinda Srinu <gsrinu0307@gmail.com>
2026-01-29 21:55:38 +0530

    db.js

--- db.js
+++ db.js
@@ -2,17 +2,7 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-  connectionTimeoutMillis: 5000,
+  ssl: { rejectUnauthorized: false } // important for cloud DBs
 });
-
-(async () => {
-  try {
-    await pool.query("SELECT 1");
-    console.log("✅ Database connected");
-  } catch (err) {
-    console.error("❌ DB error:", err.message);
-  }
-})();
 
 module.exports = pool;
